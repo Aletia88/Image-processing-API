8fa270e4b1f12577bb7fc63e5a13fd486daa9667
Aletia88 <abababmgmgmg@gmail.com>
2022-12-30 23:25:52 +0300

    image processing api udaciy

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,13 @@
 import express from 'express';
 const app = express();
-import route from './routes/index.'
-import path from 'path';
-import { resizeImage } from './utilities/resize';
-
+import route from './routes/index.';
 
 const port = 3000;
-
 
 app.use('/api', route);
 
 app.listen(port, () => {
-    console.log('the server is started')
+  console.log('the server is started');
 });
 
 export default app;
--- src/test/indexSpec.ts
+++ src/test/indexSpec.ts
@@ -3,42 +3,25 @@ import app from '../index';
 
 const request = supertest(app);
 
+it('returns 404 for invalid endpoint', async () => {
+  const response: supertest.Response = await request.get('/unknown');
 
-describe('endpoint: /foo', (): void => {
-    it('returns 404 for invalid endpoint', async (): Promise<void> => {
-      const response: supertest.Response = await request.get('/foo');
+  expect(response.status).toBe(404);
+});
 
-      expect(response.status).toBe(404);
-    });
-  });
+it('checks for missing query parameters', async () => {
+  const response = await request.get('/api/images?filename=fjord');
+  expect(response.status).toBe(200);
+});
 
-  it('checks for missing query parameters', async () => {
-        const response = await request.get('/api/images?filename=fjord');
-        expect(response.status).toBe(200);
-      });
-    
+it('checks for correct query parameters name', async () => {
+  const response: supertest.Response = await request.get('/api/images?filename=fjord');
 
-  describe('Test responses from endpoints', (): void => {
-    
-  
-    describe('endpoint: /api/images', (): void => {
-      it('gets /api/images?filename=fjord (valid args)', async (): Promise<void> => {
-        const response: supertest.Response = await request.get(
-          '/api/images?filename=fjord'
-        );
-  
-        expect(response.status).toBe(200);
-      });
-  
-      it('checks for valid input', async (): Promise<void> => {
-        const response: supertest.Response = await request.get(
-          '/api/images?filename=fjord&width=200&height=200'
-        );
-  
-        expect(response.status).toBe(200);
-      });
-  
-    
-     
-    });
-})
+  expect(response.status).toBe(200);
+});
+
+it('checks for valid input', async () => {
+  const response: supertest.Response = await request.get('/api/images?filename=fjord&width=200&height=200');
+
+  expect(response.status).toBe(200);
+});
