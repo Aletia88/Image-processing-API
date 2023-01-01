867104c617cf82516c9aca5c99503bef090ef7d3
Aletia88 <abababmgmgmg@gmail.com>
2023-01-01 05:53:38 +0300

    Image proccesing Api

--- dist/routes/index..js
+++ dist/routes/index..js
@@ -9,24 +9,31 @@ const fs_1 = __importDefault(require("fs"));
 const route = express_1.default.Router();
 route.get('/images', (req, res) => {
     const url = `/src/images/thumb/${req.query.filename}-${req.query.width}-${req.query.height}.jpg`;
+    const imageUrl = `/src/images/images/${req.query.filename}.jpg`;
     if (Object.keys(req.query).length > 3) {
-        res.send('please enter only filename, width and height query string');
+        res.send('please enter only filename, width and height of the image');
+    }
+    else if (!fs_1.default.existsSync(imageUrl)) {
+        res.send('Please enter the correct file name');
     }
     else if (req.query.filename == undefined || req.query.width == undefined || req.query.height == undefined) {
-        res.send('enter int');
+        res.send('Sorry There is a missing parameter ');
     }
-    else if (isNaN(req.query.width)) {
-        res.send('enter only number');
+    else if (isNaN(req.query.width) || req.query.width < 1) {
+        res.send('width can only be a postive number');
     }
-    else if (Object.keys(req.query).length != 3) {
-        res.send('please enter a filename');
+    else if (isNaN(req.query.height) || req.query.height < 1) {
+        res.send('height can only be a postive number');
     }
     else if (fs_1.default.existsSync(url)) {
         res.sendFile(process.cwd() + url);
     }
-    else {
+    else if (!fs_1.default.existsSync(url) && req.query.filename) {
         (0, resize_1.resizeImage)(req.query.filename, Number(req.query.width), Number(req.query.height));
         res.sendFile(process.cwd() + url);
+    }
+    else {
+        res.sendFile("can't find the image");
     }
 });
 exports.default = route;
--- src/routes/index..ts
+++ src/routes/index..ts
@@ -8,19 +8,24 @@ route.get(
 
   (req: { query: { filename: string; width: number; height: number } }, res) => {
     const url = `/src/images/thumb/${req.query.filename}-${req.query.width}-${req.query.height}.jpg`;
+    const imageUrl = `/src/images/images/${req.query.filename}.jpg`;
     if (Object.keys(req.query).length > 3) {
-      res.send('please enter only filename, width and height query string');
+      res.send('please enter only filename, width and height of the image');
+    } else if (!fs.existsSync(imageUrl)) {
+      res.send('Please enter the correct file name');
     } else if (req.query.filename == undefined || req.query.width == undefined || req.query.height == undefined) {
-      res.send('enter int');
-    } else if (isNaN(req.query.width)) {
-      res.send('enter only number');
-    } else if (Object.keys(req.query).length != 3) {
-      res.send('please enter a filename');
+      res.send('Sorry There is a missing parameter ');
+    } else if (isNaN(req.query.width) || req.query.width < 1) {
+      res.send('width can only be a postive number');
+    } else if (isNaN(req.query.height) || req.query.height < 1) {
+      res.send('height can only be a postive number');
     } else if (fs.existsSync(url)) {
       res.sendFile(process.cwd() + url);
-    } else {
+    } else if (!fs.existsSync(url) && req.query.filename) {
       resizeImage(req.query.filename, Number(req.query.width), Number(req.query.height));
       res.sendFile(process.cwd() + url);
+    } else {
+      res.sendFile("can't find the image");
     }
   }
 );
